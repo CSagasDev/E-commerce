6eec61bf0951a38ea2af50e9df6bcc315773294d
CSagasDev <c.sagastume.dev@gmail.com>
2026-02-05 16:26:30 -0600

    DbContext Creacion de tablas

--- Ecommerce/Entities/Category.cs
+++ Ecommerce/Entities/Category.cs
@@ -7,5 +7,6 @@ namespace Ecommerce.Entities
         public int CategoryId { get; set; }
         [Required]
         public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
