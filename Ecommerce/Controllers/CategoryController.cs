4e0607fbef110f12d90196c5d72d545bbaa8c5cc
CSagasDev <c.sagastume.dev@gmail.com>
2026-02-11 14:59:44 -0600

    Primer controlador y vista creados

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class CategoryController(AppDbContext _dbContext) : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = _dbContext.Category.ToList();
+            return View(categories);
+        }
+    }
+}
--- Ecommerce/obj/Debug/net10.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net10.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06db110b22e31d930c9f7caa9efc7d7f506ecb79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640bfd18ae88fd22d148f2360006b902d64f941e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
