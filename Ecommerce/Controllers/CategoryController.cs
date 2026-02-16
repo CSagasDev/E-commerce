15dbb121d05bfbcf20a0abd63e6ee42bd6c7baae
CSagasDev <c.sagastume.dev@gmail.com>
2026-02-16 13:41:42 -0600

    Fontawesome integrado, Primer model creado

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.Context;
+using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Controllers
@@ -7,7 +8,12 @@ namespace Ecommerce.Controllers
     {
         public IActionResult Index()
         {
-            var categories = _dbContext.Category.ToList();
+            var categories = _dbContext.Category.Select(item =>
+            new CategoryVM
+            {
+                CategoryId = item.CategoryId,
+                Name = item.Name,
+            }).ToList();
             return View(categories);
         }
     }
--- Ecommerce/obj/Debug/net10.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net10.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640bfd18ae88fd22d148f2360006b902d64f941e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0607fbef110f12d90196c5d72d545bbaa8c5cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
